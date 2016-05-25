f6028b438cd0b55cc60bd338a1697644f939769f
wouter89 <w.g.stikkelorum@gmail.com>
2016-05-25 15:32:08 +0200

    Update ConditionNonBoolean.java

--- src/ql/issue/problem/ConditionNonBoolean.java
+++ src/ql/issue/problem/ConditionNonBoolean.java
@@ -1,10 +1,7 @@
 package ql.issue.problem;
 
-import ql.ast.expression.Expression;
-import ql.ast.type.ValueType;
-
 public class ConditionNonBoolean extends Problem {
-	public ConditionNonBoolean(Expression expression, ValueType expectedType, ValueType actualType) {
+	public ConditionNonBoolean(int expression, String expectedType, String actualType) {
 		super.errorMessage = String
 				.format("Invalid type for condition. Expected Type: %s. Actual type: %s. On line number: %d",
 						expectedType, actualType, expression.getLineNumber());
