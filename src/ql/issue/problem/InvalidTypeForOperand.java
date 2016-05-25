58ec1adfbf043ecd5653c3749d75bb06bad8f00a
wouter89 <w.g.stikkelorum@gmail.com>
2016-05-25 15:30:42 +0200

    Update InvalidTypeForOperand.java

--- src/ql/issue/problem/InvalidTypeForOperand.java
+++ src/ql/issue/problem/InvalidTypeForOperand.java
@@ -1,10 +1,7 @@
 package ql.issue.problem;
 
-import ql.ast.expression.Expression;
-import ql.ast.type.ValueType;
-
 public class InvalidTypeForOperand extends Problem {
-	public InvalidTypeForOperand(Expression expression, ValueType expectedType, ValueType actualType) {
+	public InvalidTypeForOperand(int lineNumberExpression, String expectedType, String actualType) {
 		super.errorMessage = String
 				.format("Invalid type for operant. Expected Type: %s. Actual type: %s. On line number: %d",
 						expectedType, actualType, expression.getLineNumber());
